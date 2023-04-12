98e59dde0efe46eda86abf83369578e5c4c13cfa
Nathan <nathan.marques176@gmail.com>
2023-04-12 07:24:24 -0300

    feat: settings env

--- fumolanches_back/settings.py
+++ fumolanches_back/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p%^gs1&r#z_xxrr&3772$z6=*^sn#&qrr8(zvp5zi74rfyj^n^'
+# get secret key from environment variable
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
